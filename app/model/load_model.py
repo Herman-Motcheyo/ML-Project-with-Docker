f065fbb348e1a51fb0952d0c64b5e9840fa1ad6a
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2025-04-15 22:04:51 +0100

    add load model file

--- app/model/load_model.py
+++ app/model/load_model.py
@@ -1,5 +1,9 @@
 import joblib
-from app.config import MODEL_PATH
+
+from app.utils import load_params
+
+params = load_params()
+MODEL_PATH = params["MODEL_PATH"]
 
 def load_model():
     return joblib.load(MODEL_PATH)
